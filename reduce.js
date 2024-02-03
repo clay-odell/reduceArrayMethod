e3a81f2357fe8370f2b0f5a8443cc8c12930d826
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-03 11:47:26 -0600

    vowelCount function and answers added

--- reduce.js
+++ reduce.js
@@ -7,3 +7,24 @@ function extractValue(arr, key) {
 const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
 
 reduceAnswer1 = extractValue(arr, 'name')
+
+function vowelCount(str){
+    const vowels = 'aeiou';
+    return str.split('').reduce(function(acc, currentVal){
+        let lowerCased = currentVal.toLowerCase();
+        if(vowels.indexOf(lowerCased) !== -1) {
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            } else {
+                acc[lowerCased] = 1;
+            }
+          }
+          return acc;
+        }, {});
+}
+reduceAnswer2 = vowelCount('Elie') 
+reduceAnswer3 = vowelCount('Tim') 
+reduceAnswer4 = vowelCount('Matt') 
+reduceAnswer5 = vowelCount('hmmm') 
+reduceAnswer6 = vowelCount('I Am awesome and so are you') 
+    
